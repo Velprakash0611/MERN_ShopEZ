0f1b1aff6d376f5986a92b9cda6a65295c762295
Velprakash0611 <velprakash0611@gmail.com>
2024-11-22 22:07:45 +0530

    Initial commit

---
+++ client/src/App.js
@@ -0,0 +1,65 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar'
+import Home from './pages/Home';
+import Authentication from './pages/Authentication';
+
+import Cart from './pages/customer/Cart';
+import Profile from './pages/customer/Profile';
+import CategoryProducts from './pages/customer/CategoryProducts';
+import IndividualProduct from './pages/customer/IndividualProduct';
+
+import Admin from './pages/admin/Admin';
+import AllProducts from './pages/admin/AllProducts';
+import AllUsers from './pages/admin/AllUsers';
+import AllOrders from './pages/admin/AllOrders';
+import NewProduct from './pages/admin/NewProduct';
+import UpdateProduct from './pages/admin/UpdateProduct';
+import logo from "./logo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a 
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferre"
+        >
+          Learn React
+        </a>
+      </header>
+      
+      <Navbar />
+      
+      <Routes>
+
+        <Route path='/auth' element={<Authentication />} />
+
+        <Route exact path='' element={<Home />}/>
+        <Route path='/cart' element={<Cart />} />
+        <Route path='/product/:id' element={<IndividualProduct />} />
+        <Route path='/category/:category' element={<CategoryProducts />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/auth' element={<Authentication />} />
+
+        <Route path='/admin' element={<Admin />} />
+        <Route path='/all-products' element={<AllProducts />} />
+        <Route path='/all-users' element={<AllUsers />} />
+        <Route path='/all-orders' element={<AllOrders />} />
+        <Route path='/new-product' element={<NewProduct />} />
+        <Route path='/update-product/:id' element={<UpdateProduct />} />
+
+      </Routes>
+
+    </div>
+  );
+}
+
+export default App;
